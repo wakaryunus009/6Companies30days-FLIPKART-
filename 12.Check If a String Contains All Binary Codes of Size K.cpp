47c54e8efe95438910a0448be79b2e8ab8e0c3b8
WAKAR YUNUS <82135292+wakaryunus009@users.noreply.github.com>
2023-01-19 21:28:05 +0530

    Add files via upload

---
+++ 12.Check If a String Contains All Binary Codes of Size K.cpp
@@ -0,0 +1,19 @@
+//Check If a String Contains All Binary Codes of Size K
+class Solution {
+public:
+   bool hasAllCodes(string str, int k) {
+    int total = 1<<k;
+    int oner = total-1;
+    int curr = 0;
+    unordered_set<int>s;
+    
+    for(int i=0;i<str.size();i++)
+    {
+        curr = (((curr<<1)&oner)|(str[i]-'0'));
+        if(i>=k-1)
+            s.insert(curr);
+    }
+    
+    return (s.size()==total);
+}
+};
